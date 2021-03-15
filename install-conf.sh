b71de54b51cefe9ee80e0f42d16778937864363a
Robert Sokol <robersokol.om@gmail.com>
2021-03-15 19:38:27 +0100

    Fix parameter passing to install script

--- install-conf.sh
+++ install-conf.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 dotconf() {
-    git --git-dir="$HOME"/.cfg/ --work-tree="$HOME"
+    git --git-dir="$HOME"/.cfg/ --work-tree="$HOME"/ "$@"
 }
 
-echo ".cfg" >> "$HOME"/.gitignore
+echo ".cfg" > "$HOME"/.gitignore
 
 mkdir -p "$HOME"/.config-backup && \
 dotconf checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | \
