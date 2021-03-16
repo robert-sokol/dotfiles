b62a693130c3f02d020ce5cb70437df5d2e12aa2
Robert Sokol <robersokol.om@gmail.com>
2021-03-16 07:38:43 +0100

    Update install script for vim plugin handling

--- install-conf.sh
+++ install-conf.sh
@@ -8,10 +8,11 @@ echo ".cfg" > "$HOME"/.gitignore
 
 mkdir -p "$HOME"/.config-backup
 mkdir -p "$HOME"/.cfg
+mkdir -p "$HOME"/.vim/autoload
 
 for _file in $(git ls-tree -r main --name-only); do
     [[ -f "$HOME"/"$_file" ]] && mv "$HOME"/"$_file" "$HOME"/.config-backup/
-    cp -f "$_file" "$HOME"/
+    cp -f "$_file" "$HOME"/"$_file"
 done
 rsync -a .git/ "$HOME"/.cfg/
 
